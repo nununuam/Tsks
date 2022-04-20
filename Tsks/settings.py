aa6fc6235a96eaae0a6328d12f8a873383f5f583
nununuam <nunusnuam@gmail.com>
2022-04-20 13:30:43 -0700

    just added the .env file

--- Tsks/settings.py
+++ Tsks/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2avx)mp9)m*)+(eb&3hz54vofq02r*4#qo*b7&3h5u757c@u&u'
-
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
