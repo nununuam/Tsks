c7b5bc6369eadd18bf1b758fafd48f3d50c43d9a
nununuam <nunusnuam@gmail.com>
2022-04-20 13:17:58 -0700

    playing with fake database to see it render out on html

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,25 @@ from django.views.generic.base import TemplateView
 
 
 # Create your views here.
+class Cat:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
 
+cats = [
+    Cat("Mau", 5, "Female"),
+    Cat("Garfield", 43, "Male"),
+    Cat("Meowth", 25, "Male"),
+    Cat("Salem", 500, "Male"),
+]
+
+class CatList(TemplateView):
+    template_name = 'catlist.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cats"] = cats # this is where we add the key into our context object for the view to use
+        return context
 
 class Home(TemplateView):
     template_name = "home.html"
