f5735f3835f8aba0b90e2ac086bd180100db760f
nununuam <nunusnuam@gmail.com>
2022-04-20 19:21:22 -0700

    models is done

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Categories(models.Model):
+    CATEGORIES = [
+        ('Personal', 'Personal'),
+        ('Errands', 'Errands'),
+        ('Work', 'Work'),
+        ('Friends', 'Friends'),
+        ('Special Occasion', 'Special Occasion'),
+        ('Classes', 'Classes'),
+        ('Extracricular', 'Extracricular'),
+        ('Random', 'Random'),
+        ('Un-assigned', 'Un-assigned')
+    ]
+    categories = models.CharField(
+        max_length=16,
+        choices=CATEGORIES,
+    )
+
+class Task(models.Model):
+    categories = models.ManyToManyField(Categories)
+    title = models.CharField(max_length = 60)
+    discription = models.CharField(max_length = 250)
+    date_time = models.DateTimeField()
+    complete = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
